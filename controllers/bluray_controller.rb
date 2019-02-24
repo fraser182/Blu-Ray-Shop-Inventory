1646a56b0483e7bd32996a01ea7c4a3f21281292
Fraser Ross <fraser182@icloud.com>
2019-02-24 13:39:31 +0000

    adjusted all stock links

--- controllers/bluray_controller.rb
+++ controllers/bluray_controller.rb
@@ -1,12 +1,6 @@
 require_relative('../models/bluray.rb')
 require_relative('../models/supplier.rb')
 
-# SHOW ALL
-# get '/blurays' do
-#   @blurays = BluRay.all
-#   erb(:"blurays/index")
-# end
-
 # NEW
 get '/blurays/new' do
   @suppliers = Supplier.all
@@ -25,28 +19,6 @@ if params[:stock_level] == "out_of_stock"
       erb(:index)
     end
 end
-
-
-
-# out of stock needs to be above /blurays/:id
-# get '/blurays/out_of_stock' do
-#   @blurays = BluRay.list_out_of_stock()
-#   erb(:"/blurays/out_of_stock")
-# end
-
-
-# in stock needs to be above /blurays/:id
-# get '/blurays/in_stock' do
-#   @blurays = BluRay.list_in_stock()
-#   erb(:"/blurays/in_stock")
-# end
-
-
-# low stock needs to be above /blurays/:id
-# get '/blurays/low_stock' do
-#   @blurays = BluRay.list_low_stock()
-#   erb(:"/blurays/low_stock")
-# end
 
 # SHOW
 get '/blurays/:id' do
